0c38a6e0cc415d44a20851262bcf0d07d3552c8c
Epulapp <Epulapp@epulapp120.univ-lyon1.fr>
2018-12-21 09:21:16 +0100

    Creation map, bouton, label

--- src/components/containers/Maps.js
+++ src/components/containers/Maps.js
@@ -19,7 +19,7 @@ class Maps extends Component {
          return (
                 <Map className="map" center={position} zoom={this.state.zoom}>
                         <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                                <Marker position={position}>
+                                <Marker className="marker" position={position}>
                                       <Popup>A pretty CSSS3 popup !</Popup>  
                                 </Marker>
                 </Map>   
