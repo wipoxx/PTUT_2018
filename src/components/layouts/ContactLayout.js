8d6a243dfc642a183f1367837aadc63f5d3b9b57
xSirFrost <nottonq@gmail.com>
2019-01-20 14:33:08 +0100

    QNN - save pour pas tout perdre (je corrige les pb plus tard)

--- src/components/layouts/ContactLayout.js
+++ src/components/layouts/ContactLayout.js
@@ -1,11 +1,57 @@
 import React, { Component } from "react";
+import "./ContactLayout.css";
+import { Editor } from '@tinymce/tinymce-react';
 
 class ContactLayout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {valueNom: '', valueMail:'', valueTexte:''};
+
+        this.handleChangeNom = this.handleChangeNom.bind(this);
+        this.handleChangeMail = this.handleChangeMail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEditorChange = (e) => {
+        this.setState({valueTexte: e.target.getContent()});
+    }
+
+    handleChangeNom(event) {
+        this.setState({valueNom: event.target.value});
+    }
+
+    handleChangeMail(event) {
+        this.setState({valueMail: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: '+ this.state.valueNom + '\n A email was submitted: '+ this.state.valueMail+ '\n A text was submitted: '+ this.state.valueTexte);
+        event.preventDefault();
+    }
+
 	render() {
 		return (
-			<div>
-				<h1>Contact</h1>
-			</div>
+            <div className="contact-fond">
+                <h1 className="contacttitre1">une <b> question </b> ? contacter nous !</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label className="contacttext1">
+                        Nom : <input type="text" className="contactwidht" value={this.state.valueNom} onChange={this.handleChangeNom}/>
+                    </label>
+                    <label className="contacttext2">
+                        Email : <input type="email" className="contactwidht" value={this.state.valueMail} onChange={this.handleChangeMail}/>
+                    </label >
+					<div className="contacttext3">
+                    <Editor
+                        apiKey='zlmfex2571l9j2z3zr1eq1p0qv082541562m2i3fe03hv9oi '
+                        init={{
+                            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright'
+                        }}
+                        onChange={this.handleEditorChange}
+                    />
+					</div>
+                    <input className="contactbutton1" type="submit" value="Envoyer" />
+                </form>
+            </div>
 		);
 	}
 }
--- src/components/layouts/HomeLayout.js
+++ src/components/layouts/HomeLayout.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import "./HomeLayout.css";
 
 class HomeLayout extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Accueil</h1>
+            <div className="home-fond">
+				<h1 className="titre1">Un emplacement <b> parfait</b> pour une entreprise ?</h1>
+				<p className="text1">Ne vous poser plus de question ! <b> Niglow </b> est la pour vous.</p>
+                <p className="text2">Grace a sa carte interactive, trouver l'emplacement idéal pour votre entreprise</p>
+				<button className="button1">essayer maintenant</button>
 			</div>
 		);
 	}
