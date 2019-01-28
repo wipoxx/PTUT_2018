e6ba8363812a8c8f430d5678d6f731470767479d
Epulapp <Epulapp@epulapp120.univ-lyon1.fr>
2019-01-28 17:23:19 +0100

    Map avec cluster

--- src/components/containers/Maps.js
+++ src/components/containers/Maps.js
@@ -1,34 +1,81 @@
 import React, { Component } from 'react';
+import L from "leaflet";
 import "../../../node_modules/leaflet/dist/leaflet.css";
-import "../../../node_modules/leaflet/dist/leaflet.js";
+// import "../../../node_modules/leaflet/dist/leaflet.js";
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import "./Maps.css";
+import MarkerClusterGroup from 'react-leaflet-markercluster/dist/react-leaflet-markercluster';
+import 'react-leaflet-markercluster/dist/styles.min.css';
+import 'leaflet/dist/leaflet.css?url=false';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'react-leaflet-markercluster/dist/styles.min.css';
+
+var myIcon = L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon-2x.png',
+    iconSize: [25, 40],
+    });
 
 class Maps extends Component {
     constructor(props) {
         super(props);
         this.state = {
             //Latitude
-            lat: 48.862725,
+            lat: 45.764043,
             //Longitude
-            lng: 2.287592000000018,
+            lng: 4.835659,
             //Zoom de base sur la map
-            zoom: 6,
+            zoom: 10,
+            //Zoom maxi sur la map
+            maxZoom: 18,
         }
     }
     render() {
         //Définition de la position entière
          const position = [this.state.lat, this.state.lng];
          return (
-                <Map className="map" center={position} zoom={this.state.zoom}>
+                <Map className="map" center={position} zoom={this.state.zoom} maxZoom={this.state.maxZoom}>
                         <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                                <Marker position={position}>
-                                      <Popup>A pretty CSSS3 popup !</Popup>
-                                </Marker>
+                        <MarkerClusterGroup iconCreateFunction={createClusterCustomIcon}>
+                               
+                         {this.props.donneesMap.map(function(donnee){
+                        return <Marker position={[donnee.lat, donnee.long]} icon={myIcon}>
+                        <Popup>{donnee.nom} <br/>{donnee.activite}</Popup>
+                        </Marker>
+                        })
+                    }
+                        </MarkerClusterGroup>
                 </Map>
         )
         
     }
+
  }
+
+ const createClusterCustomIcon = (cluster) => {
+    const count = cluster.getChildCount();
+    let size = 'LargeXL';
+  
+    if (count < 5) {
+      size = 'Small';
+    }
+    else if (count >= 5 && count < 50) {
+      size = 'Medium';
+    }
+    else if (count >= 50 && count < 100) {
+      size = 'Large';
+    }
+    const options = {
+      cluster: `markerCluster${size}`,
+    };
+  
+    return L.divIcon({
+      html:
+        `<div>
+          <span class="markerClusterLabel ${options.cluster}">${count}</span>
+        </div>`,
+      className: `${options.cluster}`,
+    });
+  };
 
 export default Maps;
--- src/components/layouts/MapLayout.js
+++ src/components/layouts/MapLayout.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import Formulaire from "../containers/Formulaire";
 import Maps from "../containers/Maps";
+import ListeDeroulante from "../presentation/ListeDeroulante";
+
 
 export default class MapLayout extends Component {
 	render() {
+		var mesDonnees;
+		var mesDonneesNow;
+		var donnees=[
+			{"id":"1", "activite":"Boulangerie", "nom":"Boulangerie Menetrier", "Adresse":"115 Avenue Roger Salengro, 69100 Villeurbanne", "lat":"45.7784646", "long":"4.879230499999949"},
+			{"id":"2", "activite":"Boulangerie", "nom":"Aux Milles et Une Mie", "Adresse":"38 Avenue Roger Salengro, 69100 Villeurbanne", "lat":"45.7760103", "long":"4.871306499999946"},
+			{"id":"3", "activite":"Boulangerie", "nom":"La Huche Aux Pains", "Adresse":"78 Avenue Roger Salengro, 69100 Villeurbanne", "lat":"45.776613", "long":"4.876162399999998"},
+			{"id":"4", "activite":"Boulangerie", "nom":"J&H Boulangerie Tonkin", "Adresse":"2 rue Salvador Allende, Centre Commercial Samouraï, 69100 Villeurbanne", "lat":"45.776074", "long":"4.8622857000000295"},
+			{"id":"5", "activite":"Supermarché", "nom":"Express", "Adresse":"110 Boulevard du 11 Novembre 1918, 69100 Villeurbanne", "lat":"45.776074", "long":"4.8622857000000295"},
+			{"id":"6", "activite":"Supermarché", "nom":"Carrefour Market", "Adresse":"61 Avenue Roger Salengro, 69100 Villeurbanne", "lat":"45.7770772", "long":"4.875454200000036"},
+			{"id":"7", "activite":"Supermarché", "nom":"Casino Shop", "Adresse":"2 Avenue Salvador Allende, 69100 Villeurbanne", "lat":"45.77601399999999", "long":"4.862334000000033"},
+			{"id":"8", "activite":"Supermarché", "nom":"Franprix", "Adresse":"21 Avenue Piaton, 69100 Villeurbanne", "lat":"45.77426620000001", "long":"4.867366899999979"}
+		]
+		var typeActivite=[
+			{"id":"1", "activite":"Boulangerie"},
+			{"id":"2", "activite":"Supermarché"},
+		]
 		return (
 			<div>
-				<Maps/>
+				<Maps donneesMap={donnees} />
+				<ListeDeroulante typeActivite={typeActivite} onSumbit={this.handleSubmit}/>
+				
 			</div>
 		)
-	}
-
-	handleSubmit = (valeur) => {
-		
+	
 	}
 }
