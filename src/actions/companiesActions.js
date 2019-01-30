48de1866a2efe66bbd312cc712a4f32cc8330e77
powermask <mehdi.labourdette@gmail.com>
2019-01-30 16:46:24 +0100

    Ajout de la récupération des stats sur les activités des entreprises

--- src/actions/companiesActions.js
+++ src/actions/companiesActions.js
@@ -10,6 +10,20 @@ export function loadCompanies(params) {
     };
 }
 
+export function loadCompaniesStatsActivities(params) {
+    return function(dispatch) {
+        return companiesApi.getCompaniesStatsActivities(params).then(activities => {
+            dispatch(loadCompaniesStatsActivitiesSuccess(activities));
+        }).catch(error => {
+            throw(error);
+        });
+    };
+}
+
 export function loadCompaniesSuccess(companies) {
     return {type: 'LOAD_COMPANIES_SUCCESS', companies};
+}
+
+export function loadCompaniesStatsActivitiesSuccess(activities) {
+    return {type: 'LOAD_COMPANIES_STATS_ACTIVITIES_SUCCESS', activities};
 }
